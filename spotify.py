6b1d2d123aa59f4f84bfc6bce87c9d90c95bd2f5
Darren Jones <darrenwjones06@gmail.com>
2019-12-01 14:23:01 -0600

    idk

--- spotify.py
+++ spotify.py
@@ -2,11 +2,14 @@ import requests
 import json
 from flask import Flask, render_template
 import config
+from pyfy import Spotify
 
 app = Flask(__name__)
 #app.config.from_object('config')
 
 #client_id = config.client_id
+spt = Spotify('BQAKB6dyRzkak2ebEw0QGfjInAtFiH5nWf751_d3B0Cf2s1K-EM2etnkK1erLaC9v2rM4EJ5XexzhJ5loJRTkH6y11ZdNut6BvLy4BBOnjJjWH3nKggqZxmSF6O71w_M4C13hdrAElNZNhAj1qByKxfiprefx9LeuslAtS76AWAORXfjZ5B7uV0Z6YgAmyS_XBMcnd5gGFm5RUvcveyQelcEdMK50aqJ5Bc7FBYKHOABVeHbGYb6oz0qlocECoxQpLuCitWvgB5-QoiSal4')
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,11 +22,17 @@ def add():
     
 @app.route('/queue')
 def queue():
-    return "hello world"
+    data = []
+    results = spt.playlist_tracks(playlist_id="55UelCQIRMlZhVEKGSyUOU", fields="items(track(name,album(name),artists.name))")
+    for song in results["items"]:
+        entry = {"title": song["track"]["name"], "artist": song["track"]["artists"][0]["name"], "album":  song["track"]["album"]["name"]}
+        data.append(entry)
+        
+    return render_template('queue.html', x=data)
     
 @app.route('/recent')
 def recent():
-    return "hello world"
+    return render_template('recent.html')
     
 def addTrackToPlaylist(token, user, playlist_id, track_id, position):
     track = 'spotify:track:' + track_id
